1236b773b3f0935bdd98c35ed19fa365a00f92c3
why <b13120737820@163.com>
2025-10-08 21:28:15 -0500

    feat: add result list

--- caculator2/index.js
+++ caculator2/index.js
@@ -1,41 +1,33 @@
-        //定义变量存储用户当前输入（即上一个运算符之后，下一个运算符之前）的内容
         var string = "";
-        //数组用来存储用户在一次运算结束前的所有的数字
         var nums = new Array();
-        //数组用来存储运算符
         var maths = new Array();
-        //函数：显示当前输入
+        var resultList = [];
         function showScreen(){
             document.getElementById("userIn").innerText = string;
         }
-        //清除按钮功能：清除屏幕内容，并重置数据(当前输入内容,运算符,运算符之前的内容)
         document.getElementById("clear").onclick = function () {
             string = "";
             nums = new Array();
             maths = new Array();
-            document.getElementById("userIn").innerHTML = "<div class=\"text\">简易的计算器</div>";
-            document.getElementById("mathResult").innerHTML = "<div class=\"text\">by keats_Coder</div>";
+            document.getElementById("userIn").innerHTML = "<span class=\"text-gray\">Sample Calculator</span>";
+            document.getElementById("mathResult").innerHTML = "<span class=\"text-gray\">Let's Go</span>";
         }
-        //退格按钮功能：删除当前输入栏最后一个输入的数字
         document.getElementById("back").onclick = function () {
             if( string ){
                 string = new String(string).substring(0,string.length-1)
             }
             showScreen();
         }
-        //小数点
         document.getElementById("dot").onclick = function () {
             string = string + ".";
             showScreen();
         }
-        //正负号
         document.getElementById("minus").onclick = function(){
             if (string) {
                 string = parseInt(string) * (-1);
             }
             showScreen();
         }
-        //运算符功能，1 --> +; 2 --> -; 3 --> *; 4 --> /
         for (var i = 1; i <= 4; i++) {
             document.getElementById("operator"+i).onclick = (function (i){
                 return function() {
@@ -46,7 +38,6 @@
                 }
             })(i);
         }
-        //数字按钮
         for (i = 0; i < 10; i++) {
             document.getElementById("num"+i).onclick = (function (i) {
                 return function () {
@@ -55,43 +46,63 @@
                 }
             })(i);
         }
-        //等号按钮。运算并显示结果
+
+        function showCalcList() {
+            var fragment = document.createDocumentFragment();
+            resultList.forEach(item => {
+                const calcItem = document.createElement("div");
+                calcItem.innerHTML = item;
+                fragment.appendChild(calcItem);
+            })
+            var calcListElement = document.getElementById("calcList");
+            calcListElement.innerHTML = '';
+            calcListElement.appendChild(fragment);
+            
+        }
         document.getElementById("result").onclick = function () {
-            //定义变量-结果
             var result;
-            //如果用户参与运算的只有一个
+            let left = '';
+            var calcListItem = "";
             if (nums.length == 0) {
                 result = string;
             }else {
-                //将最后一个数存入数组
                 nums.push(string);
-                //取出数组中的值进行运算
                 for (var i = 0; i < nums.length - 1; i++) {
                     var right = nums[i + 1];
                     var math = maths[i];
                     if (i == 0) {
                         result = nums[0];
+                        left = nums[0];
                     }
                     switch (math) {
                         case 1:
                             result = parseFloat(result) + parseFloat(right);
+                            calcListItem = `${left} + ${right} = ${result}`;
                             break;
                         case 2:
                             result = parseFloat(result) - parseFloat(right);
+                            calcListItem = `${left} - ${right} = ${result}`;
                             break;
                         case 3:
                             result = parseFloat(result) * parseFloat(right);
+                            calcListItem = `${left} * ${right} = ${result}`;
                             break;
                         case 4:
-                            result = parseFloat(result) / parseFloat(right);
+                            try {
+                                result = parseFloat(result) / parseFloat(right);
+                                calcListItem = `${left} / ${right} = ${result}`;
+                            } catch (error) {
+                                
+                            }
+                            
                             break;
                     }
                 }
             }
-            //将结果安排到结果栏
             document.getElementById("mathResult").innerText = result;
-            //重置计算器
             string = "";
             nums = new Array();
             maths = new Array();
+            resultList.push(calcListItem);
+            showCalcList();
         }
