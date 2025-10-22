e15808f25dc6e86eeac426cf84907e97127be5ad
why <b13120737820@163.com>
2025-10-23 00:55:05 +0800

    feat: compile jsx

--- resume/vite.config.js
+++ resume/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          '@babel/plugin-transform-react-jsx',  // 处理 JSX 语法
+          
+        ]
+      }
+    })
+  ],
   css: {
     preprocessorOptions: {
       less: {
@@ -12,5 +22,5 @@ export default defineConfig({
       }
     }
   },
-  base: '/resume/'
-})
+  base: '/resume/'  // 设置 base 路径
+});
