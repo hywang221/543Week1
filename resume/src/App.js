f83dc0c9fbfac20ac197bab65db79202b82397f0
why <b13120737820@163.com>
2025-10-23 22:26:58 +0800

    feat: complete version 1

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.less'
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.less";
 
 import { Button } from "antd";
 
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <main>
+      <main className="container">
         <header className="header">
           <div className="logo">Logo</div>
           <div className="header-right">
@@ -26,11 +26,78 @@ function App() {
             <div>
               <img src="" className="avatar" />
             </div>
+            <div className="avatar-info">
+              <div>Hongyang Wang</div>
+            </div>
           </section>
           <section className="content-right">
-
+            <div className="detail-item">
+              <div className="detail-title">Skill Introduction</div>
+              <div className="detail-item-content">
+                Proficient in Modern front-end frameworks, including React,
+                VueJs, AngularJs.
+                <br />
+                Proficient in front-end build automation tools, including
+                Webpack and Vite.
+                <br />
+                Proficient in Node.js, Express, and MongoDB.
+                <br />
+                Proficient in CSS preprocessor technologies: SASS and LESS...
+              </div>
+              <a className="detail-more">More +</a>
+            </div>
+            <div className="detail-item">
+              <div className="detail-title">Company Introduction</div>
+              <div className="detail-item-content">
+                <span>JD.com</span>
+                <span>
+                  Led 4 front-end developers in completing the design,
+                  development, and deployment of various technical projects,
+                  such as form and table visualization and micro-frontend
+                  routing refactoring.
+                  <br />
+                  Responsible for maintaining and managing the company’s core
+                  component library, @dada/form.
+                  <br />
+                  Conducted the first round of interviews...
+                </span>
+              </div>
+              <a className="detail-more">More +</a>
+            </div>
+            <div className="detail-item">
+              <div className="detail-title">Company Introduction</div>
+              <div className="detail-item-content">
+                <span>Form and Table Visualization</span>
+                <br />
+                <span>12/2020 - 12/2023</span>
+                <br />
+                <span>
+                  Management System primarily consists of numerous lists and
+                  forms. To address this, my company provides a visual
+                  drag-and-drop editor and a configuration tool developed based
+                  on Formily framework. These tools help developers quickly
+                  build forms and list pages in the back-end management
+                  framework...
+                </span>
+              </div>
+              <a className="detail-more">More +</a>
+            </div>
           </section>
         </section>
+        <footer className="footer">
+          <div className="footer-content">
+            <div className="footer-left">
+            <div className="footer-left-title">Contact Info</div>
+            <div className="footer-left-item">
+              <span>Phone: +1-312-434-1065</span>
+              <span>Email: hwang221@hawk.illinoistech.com</span>
+
+            </div>
+          </div>
+          <div className="footer-right"></div>
+          </div>
+          
+        </footer>
       </main>
       {/* <div>
         <a href="https://vite.dev" target="_blank">
@@ -53,7 +120,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
