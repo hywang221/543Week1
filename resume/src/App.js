9fd9c4bc895bdf01dc2971466e96c68926cdccdc
why <b13120737820@163.com>
2025-12-01 23:30:27 +0800

    FP-React-Resume: Remove the code comment

--- resume/src/App.js
+++ resume/src/App.js
@@ -25,40 +25,9 @@ function App() {
           <Route path="/work" element={<Work />} />
         </Routes>
         <Footer />
-        {/* <header className="header">
-          <div className="logo">Logo</div>
-          <div className="header-right">
-            <div className="header-right-item active">Home</div>
-            <div className="header-right-item">Skill</div>
-            <div className="header-right-item">
-              <Link to="/company">Company</Link>
-            </div>
-            <div className="header-right-item">Work</div>
-            <div className="header-right-item">Contact</div>
-          </div>
-        </header> */}
         
       </main>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      
     </>
   );
 }
--- resume/src/pages/Home.js
+++ resume/src/pages/Home.js
@@ -6,13 +6,11 @@ import { Button } from "antd";
 import styles from  "./home.module.less";
 
 function Home() {
-  // const [count, setCount] = useState(0)
   const homeData = useSelector((state) => state.homeData);
   console.log('homeData', homeData)
 
   return (
     <>
-      {/* <div>{count}</div> */}
       <section className={styles.content}>
         <section className={styles.contentLeft}>
           <div className={styles.avatarWrapper}>
@@ -70,20 +68,6 @@ function Home() {
           </div>
         </section>
       </section>
-      {/* <footer className={styles.footer}>
-        <div className={styles.footerContent}>
-          <div className={styles.footerLeft}>
-            <div className={styles.footerLeftTitle}>Contact Info</div>
-            <div className={styles.footerLeftItem}>
-              <span>Phone: +1-312-434-1065</span>
-              <span>Email: hwang221@hawk.illinoistech.edu</span>
-            </div>
-          </div>
-          <div className={styles.footerRight}>
-            <img className={styles.footerCode} src={new URL(`../images/${"w_code.jpg"}`, import.meta.url).href} />
-          </div>
-        </div>
-      </footer> */}
     </>
   );
 }
